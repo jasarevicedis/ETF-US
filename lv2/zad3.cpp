e3ed9a318e885f4a77f6abdfca20b4264fe13c78
edisjasarevic <e.jasarevic03@gmail.com>
2023-03-19 08:46:17 +0100

    Create zad3.cpp

---
+++ lv2/zad3.cpp
@@ -0,0 +1,30 @@
+#include "mbed.h"
+
+BusOut myleds(p5,p6,p7,p8,p9,p10,p11,p12);
+int dec;
+double increasing_factor = 2;
+
+int main() {
+    myleds=1;
+    wait(1);
+    while(1){
+        dec=2;
+        for(int i=0;i <6; i++){
+            myleds = dec;
+            dec *= increasing_factor;
+            wait(1);
+        }
+        myleds = 255;
+        wait(1);
+        
+        dec/=2;
+        for(int i=0; i< 6; i++){
+            myleds = dec;
+            dec /= increasing_factor;
+            wait(1);
+        }
+        myleds = 255;
+        wait(1);
+    }
+
+}
